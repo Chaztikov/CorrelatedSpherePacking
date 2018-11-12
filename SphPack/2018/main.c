fd446a8d3a6144f9552c42c79c026cf87c81b1db
chaztikov <chaztikov@gmail.com>
2018-11-12 05:47:11 -0500

    updated C packing generation

---
+++ SphPack/2018/main.c
@@ -0,0 +1,13 @@
+#include "f2c.h"
+
+int main()
+{
+/*
+Please refer to makefile and rnpack.c 
+(generated from rnpack.f/rnpack.f90/rnpack.f95)
+
+note: from rnpack.c (see its final lines), can call the C analogue of F77 Program rnpack via   
+	randompack_();	
+*/
+	return 0;
+}
